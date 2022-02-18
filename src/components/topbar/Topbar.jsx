bcbfb26516acaa7af1079361ccf4f39e88d28ca8
Matthew Brown <mbrown@geomarvel.com>
2022-02-17 19:22:24 -0500

    mu-icons and contact info added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.scss";
-
+import { Person, Mail } from "@material-ui/icons";
 export default function Topbar() {
   return (
     <div className="topbar" id="topbar">
@@ -8,7 +8,14 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             brown.
           </a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>(240) 204-2927</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>matthewbrown0798@gmail.com</span>
+          </div>
         </div>
 
         <div className="right"></div>
