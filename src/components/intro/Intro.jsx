ae4f356f7a654ccfa2f326ffa7ce2d650f39f45e
Matthew Brown <mbrown@geomarvel.com>
2022-02-17 20:14:52 -0500

    intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,23 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/bit.png" alt="bitmoji" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Matthew Brown</h1>
+          <h3>
+            Software Developer<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="Down" />
+        </a>
+      </div>
     </div>
   );
 }
