7377c63e71f840d6808ee4159da9f6fbcc75bc9b
Matthew Brown <mbrown@geomarvel.com>
2022-02-17 19:44:34 -0500

    animated sidebar added

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,24 @@
+import "./menu.scss";
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu " + (menuOpen && "active")}>
+      <ul className="">
+        <li>
+          <a href="#intro">Home</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#works">Works</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
