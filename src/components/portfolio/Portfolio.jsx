1fdf4c478585fe5afbd2f736576b033458b46c71
Matthew Brown <mbrown@geomarvel.com>
2022-02-21 10:51:53 -0500

    start buildout of portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,44 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      Port
+      <h1></h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/1000/1*-7GOYVGKYbcuacgVHjKtjQ@2x.jpeg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/1000/1*-7GOYVGKYbcuacgVHjKtjQ@2x.jpeg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>{" "}
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/1000/1*-7GOYVGKYbcuacgVHjKtjQ@2x.jpeg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>{" "}
+        <div className="item">
+          <img
+            src="https://miro.medium.com/max/1000/1*-7GOYVGKYbcuacgVHjKtjQ@2x.jpeg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
